379a799b22952424ee3c7014e8c5a480d91decf4
Gustavo Loiola Fonteles <68648344+gu5t4vo@users.noreply.github.com>
2022-11-25 15:51:20 -0300

    Delete GoalkeeperEvaluationSystemSwing/src directory

--- GoalkeeperEvaluationSystemSwing/src/components/DefButton.java
+++
@@ -1,8 +0,0 @@
-package components;
-
-import javax.swing.*;
-import java.awt.event.ActionListener;
-
-public abstract class DefButton extends JButton{
-
-}
--- GoalkeeperEvaluationSystemSwing/src/components/DefPanel.java
+++
@@ -1,7 +0,0 @@
-package components;
-
-import javax.swing.*;
-
-public abstract class DefPanel extends JPanel {
-
-}
